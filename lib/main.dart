7f163e74b43c10e2cf52a6c6c4424b5c3c1f0281
Bengi Agcal <io.enceladus@gmail.com>
2021-03-22 11:45:14 +0800

    Revert "Revert "Update main.dart""
    This reverts commit 828fc19af46c28c8ec7b475f90a79e789b575d4e.

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'What is this? AX'),
+      home: MyHomePage(title: 'What is this? AX 123'),
     );
   }
 }
