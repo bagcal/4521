d1ae002faab134dcd11f8d0dd8f2a8cd180a0992
Bengi Agcal <io.enceladus@gmail.com>
2021-03-24 16:22:03 +0800

    Authentication with email.
    Authentication with email is added

--- lib/screens/Authentication/authenticate.dart
+++ lib/screens/Authentication/authenticate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'SignIn.dart';
+import 'Register.dart';
 
 class Authenticate extends StatefulWidget {
   @override
@@ -7,10 +8,20 @@ class Authenticate extends StatefulWidget {
 }
 
 class _AuthenticateState extends State<Authenticate> {
+
+  bool showSignIn = true;
+
+  void toggleView(){
+    //print(showSignIn.toString());
+    setState(() => showSignIn = !showSignIn);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SignIn(),
-    );
+    if (showSignIn) {
+      return SignIn(toggleView:  toggleView);
+    } else {
+      return Register(toggleView:  toggleView);
+    }
   }
 }
